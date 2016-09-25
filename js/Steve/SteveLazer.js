254320bef7bf30c09755785c6f7d1fbc56db0293
Everett Moser <foolsboy137@gmail.com>
2016-09-25 13:43:01 -0500

    steeeve

--- js/Steve/SteveLazer.js
+++ js/Steve/SteveLazer.js
@@ -4,25 +4,37 @@ SteveLazer.prototype = {
 	shotTimer: 0,
 	enabled: true,
 	sprite: null,
-	speed: 800,
+	//speed: 800,
+	direction: 0,
+	rotDir: 0,
 
-	constructor: function(game, x, y, direction) {
+	constructor: function(game, x, y, direction, rotDir) {
 		this.shotTimer = 0;
 		this.enabled = true;
 		this.sprite = game.add.sprite(x,y,'SteveLazer');
 		game.physics.arcade.enable(this.sprite);
 		this.sprite.anchor.setTo(.5, 0);
 		this.sprite.ParentRef = this;
-		this.sprite.angle = direction;
+		this.sprite.angle = direction * 180 / Math.PI;
+		this.direction = direction * 180 / Math.PI;
+		this.rotDir = rotDir;
+		this.shotTimer = 0;
 	},
 
 	update: function() {
 		if (!this.enabled) return;
+
+		this.shotTimer ++;
+
+		this.sprite.angle += this.rotDir * .66;
+		if (this.shotTimer > 45) 
+			this.destroy();
 	},
 
 	destroy: function(){
 		this.enabled = false;
-		this.sprite.destroy(); // for testing purposes.
+		this.sprite.destroy(); // for testing purposes
+		this.sprite = null;
 		//score.loseHealth();
 	}
 
